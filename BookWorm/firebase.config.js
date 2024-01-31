bdf21198b9d76be1a6c588bb35dc12134d9fae7e
Sam Christensen <sam@Chicken--Waffles.local>
2024-01-31 11:41:30 -0700

    added firebase dependency

---
+++ BookWorm/firebase.config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDdLpV4nXjFf-Z62gCpNC9hqK6km6UB58s",
+    authDomain: "bookworm-338ce.firebaseapp.com",
+    projectId: "bookworm-338ce",
+    storageBucket: "bookworm-338ce.appspot.com",
+    messagingSenderId: "90311569106",
+    appId: "1:90311569106:web:d8d7c2fe041e67beda470b",
+    measurementId: "G-FBB1BW3BFM"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
