b535fb0a9b43f6cda36634755f647b4c1358b2e9
Sam Christensen <samctensen@gmail.com>
2024-02-09 10:28:07 -0700

    fixed firebase initialization and linting on pages

--- BookWorm/app/login.tsx
+++ BookWorm/app/login.tsx
@@ -1,87 +1,127 @@
-import { View, StyleSheet, TextInput, ActivityIndicator, Button, Text } from 'react-native'
-import React, { useState } from 'react'
-import { FIREBASE_AUTH } from '../firebase.config'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  Button,
+  Text,
+} from "react-native";
+import React, { useState } from "react";
+import { FIREBASE_AUTH } from "../firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
-    const [successfulUser, setMessage] = useState('')
-    const auth = FIREBASE_AUTH;
-    
-    const signIn = async () => {
-        setLoading(true);
-        try {
-            const response = await signInWithEmailAndPassword(auth, email, password);
-            setMessage("Successful sign in");
-        }
-        catch (error) {
-            console.log(error);
-            setMessage("sign in error");
-        }
-        finally {
-            setLoading(false);
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [successfulUser, setMessage] = useState("");
+  const auth = FIREBASE_AUTH;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setMessage("Successful sign in");
+    } catch (error) {
+      console.log(error);
+      setMessage("sign in error");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const signUp = async () => {
-        setLoading(true);
-        try {
-            const response = await createUserWithEmailAndPassword(auth, email, password);
-            setMessage("Successful sign up");
-        }
-        catch (error) {
-            console.log(error);
-            setMessage("sign up error");
-        }
-        finally {
-            setLoading(false);
-        }
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setMessage("Successful sign up");
+    } catch (error) {
+      console.log(error);
+      setMessage("sign up error");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <View style={styles.container}>
-            <TextInput style={styles.input} value={email} placeholder='Email' autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
-            <TextInput style={styles.input} value={password} secureTextEntry={true} placeholder='password' autoCapitalize='none' onChangeText={(text) => setPassword(text)}></TextInput>
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={email}
+        placeholder="Email"
+        autoCapitalize="none"
+        onChangeText={(text) => {
+          setEmail(text);
+        }}
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        value={password}
+        secureTextEntry={true}
+        placeholder="password"
+        autoCapitalize="none"
+        onChangeText={(text) => {
+          setPassword(text);
+        }}
+      ></TextInput>
 
-            { loading ? <ActivityIndicator size="large" color="#0000ff" />
-            : <>
-                <Button title="Login" onPress={signIn}/> 
-                <Button title="Create Account" onPress={signUp}/>
-            </> }
-            <Text>{successfulUser}</Text>
-        </View>
-    );
-}
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <Button
+            title="Login"
+            onPress={() => {
+              void (async () => {
+                await signIn();
+              })();
+            }}
+          />
+          <Button
+            title="Create Account"
+            onPress={() => {
+              void (async () => {
+                await signUp();
+              })();
+            }}
+          />
+        </>
+      )}
+      <Text>{successfulUser}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      padding: 40,
-    },
-    main: {
-      flex: 1,
-      justifyContent: "center",
-      maxWidth: 960,
-      marginHorizontal: "auto",
-    },
-    input: {
-        borderColor: "gray",
-        width: "100%",
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 10,
-      },
-    title: {
-      fontSize: 64,
-      fontWeight: "bold",
-    },
-    subtitle: {
-      fontSize: 36,
-      color: "#38434D",
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 40,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  input: {
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
 
-export default Login
+export default Login;
--- BookWorm/firebase.config.js
+++ BookWorm/firebase.config.js
@@ -1,5 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+// ts-ignore
+import {
+  initializeAuth,
+  getAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/database";
@@ -9,16 +15,19 @@ import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDdLpV4nXjFf-Z62gCpNC9hqK6km6UB58s",
-    authDomain: "bookworm-338ce.firebaseapp.com",
-    projectId: "bookworm-338ce",
-    storageBucket: "bookworm-338ce.appspot.com",
-    messagingSenderId: "90311569106",
-    appId: "1:90311569106:web:d8d7c2fe041e67beda470b",
-    measurementId: "G-FBB1BW3BFM"
+  apiKey: "AIzaSyDdLpV4nXjFf-Z62gCpNC9hqK6km6UB58s",
+  authDomain: "bookworm-338ce.firebaseapp.com",
+  projectId: "bookworm-338ce",
+  storageBucket: "bookworm-338ce.appspot.com",
+  messagingSenderId: "90311569106",
+  appId: "1:90311569106:web:d8d7c2fe041e67beda470b",
+  measurementId: "G-FBB1BW3BFM",
 };
 
 const FIREBASE = initializeApp(firebaseConfig);
+initializeAuth(FIREBASE, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 export const FIREBASE_AUTH = getAuth(FIREBASE);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
