ce9ef20469dedfbcec7380aead33188a9a97f1e3
Sam Christensen <samctensen@gmail.com>
2024-02-12 13:55:04 -0700

    removed comments

--- BookWorm/app/index.tsx
+++ BookWorm/app/index.tsx
@@ -11,13 +11,9 @@ export default function App() {
       return;
     }
     const user = FIREBASE_AUTH.currentUser;
-    // Your async logic to check user authentication can be placed here
-    // For simplicity, I'm using a synchronous check here
     if (user === null) {
-      // Navigate to the 'login' route if the user is not authenticated
       router.replace("/login");
     } else {
-      // Navigate to the 'home' route if the user is authenticated
       router.replace("/home");
     }
   }, [segments, navigationState?.key]);
