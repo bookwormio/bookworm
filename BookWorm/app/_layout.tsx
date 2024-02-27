2570ab1ce963668287d02c684a49a4a934f13d2a
nforr <nicolecforrester@gmail.com>
2024-02-27 11:25:28 -0700

    commented out some functionality for __layout and context

--- BookWorm/app/_layout.tsx
+++ BookWorm/app/_layout.tsx
@@ -2,6 +2,9 @@ import { Slot } from "expo-router";
 import React from "react";
 import { AuthenticationProvider } from "../components/auth/context";
 
+// rendering AuthenticationProvider
+// Slot is a child component within the AuthenticationProvider context
+// AuthenticationProvider is a top level component where authentication context is provided
 export default function AuthenticatedRoot() {
   return (
     <AuthenticationProvider>
--- BookWorm/components/auth/context.tsx
+++ BookWorm/components/auth/context.tsx
@@ -27,34 +27,51 @@ export function useAuth() {
 }
 
 // This hook can be used to access the user info.
+// called by AuthenticationProvider
 function useAuthenticatedRoute(user: User | null) {
   const segments = useSegments();
   const router = useRouter();
 
   React.useEffect(() => {
+    // segments allow you to accesss current route segments
     const inAuthGroup = segments[0] === "(auth)";
+    // redirects to sign in page if user DNE
     if (user == null && !inAuthGroup) {
+      // replaces path
       router.replace("/sign-in");
+      // redirects to post page if user exists
     } else if (user != null && inAuthGroup) {
+      // replaces path
       router.replace("/posts");
     }
   }, [user, segments]);
 }
 
+// this is what __layout is calling
+// props: React.PropsWithChildren allows the Authentication provider component to render and accept any children components passed to it.
 export function AuthenticationProvider(props: React.PropsWithChildren) {
   const [loading, setLoading] = useState(false);
+  // currentUser is built into FIREBASE_AUTH and is a refernece to current authenticated user
   const [currentUser, setUser] = useState(FIREBASE_AUTH.currentUser);
+  // allows you to modify a variable that exists outside of function scope
+  // modifies state of external system
+  // triggering ui updates
+  // any interaction with global environment
   useEffect(() => {
     setLoading(true);
+    // listens for change in authentication user
     const unsubscribe = FIREBASE_AUTH.onAuthStateChanged((authUser) => {
       setUser(authUser);
       setLoading(false);
     });
+    // listener is returned from useEffect to ensure that listener is removed when component no longer in use
     return unsubscribe;
   }, []);
   useAuthenticatedRoute(currentUser);
 
   return (
+    // this code sets up an authentication context using React's Context API, providing authentication-related functions and data to all components within its subtree.
+    // wraps children components accessed via props.children and passes down a 'value' prop, which contains various authentication-related functions and data
     <AuthContext.Provider
       value={{
         signIn: (email: string, password: string) => {
