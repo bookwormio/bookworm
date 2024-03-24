2703cda97ec736697e77ab2fa823a966706255ef
Riley C <rileycrahen@gmail.com>
2024-03-24 10:58:28 -0600

    outline of CRUD

--- BookWorm/types/index.d.ts
+++ BookWorm/types/index.d.ts
@@ -32,3 +32,10 @@ interface BooksResponse {
   items: BookVolumeItem[];
   totalItems: number;
 }
+
+declare enum FollowStatus {
+  FOLLOWING = "following",
+  NOT_FOLLOWING = "not following",
+  REQUESTED = "requested",
+  DENIED = "denied",
+}
