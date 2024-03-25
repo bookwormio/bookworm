a07ee06e684660baedfd8fc962dbab6b8cbe37da
Riley C <rileycrahen@gmail.com>
2024-03-25 12:16:55 -0600

    Mostly implemented

--- BookWorm/types/index.d.ts
+++ BookWorm/types/index.d.ts
@@ -32,10 +32,3 @@ interface BooksResponse {
   items: BookVolumeItem[];
   totalItems: number;
 }
-
-declare enum FollowStatus {
-  FOLLOWING = "following",
-  NOT_FOLLOWING = "not following",
-  REQUESTED = "requested",
-  DENIED = "denied",
-}
