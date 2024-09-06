17341e5f2e574be63f22edb8af46faa9cd5b0036
nforr <u1310549@utah.edu>
2024-09-06 15:32:19 -0600

    NotificationItem in its own component

--- BookWorm/app/(tabs)/(posts)/notifications.tsx
+++ BookWorm/app/(tabs)/(posts)/notifications.tsx
@@ -1,21 +1,16 @@
-import { FontAwesome5 } from "@expo/vector-icons";
-import { router } from "expo-router";
 import React, { useCallback } from "react";
 import {
   ActivityIndicator,
-  Image,
   RefreshControl,
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from "react-native";
 import Toast from "react-native-toast-message";
 import { useAuth } from "../../../components/auth/context";
 import { useGetAllFullNotifications } from "../../../components/notifications/hooks/useNotificationQueries";
-import { calculateTimeSinceNotification } from "../../../components/notifications/util/notificationUtils";
-import { NotificationType } from "../../../enums/Enums";
+import NotificationItem from "../../../components/notifications/NotificationItem";
 
 const NotificationsScreen = () => {
   const { user } = useAuth();
@@ -71,64 +66,8 @@ const NotificationsScreen = () => {
           }
         >
           {notifdata.map((notif) => {
-            const time = calculateTimeSinceNotification(notif.created.toDate());
             return (
-              <TouchableOpacity
-                key={notif.created.toString()}
-                style={styles.notif_container}
-                onPress={() => {
-                  if (
-                    notif.type === NotificationType.LIKE ||
-                    notif.type === NotificationType.COMMENT
-                  ) {
-                    router.push({
-                      pathname: `/${notif.postID}`,
-                    });
-                  } else if (notif.type === NotificationType.FRIEND_REQUEST) {
-                    router.push({
-                      pathname: `/user/${notif.sender}`,
-                    });
-                  }
-                }}
-              >
-                <View style={styles.imageTextContainer}>
-                  <View style={styles.defaultImageContainer}>
-                    {notif.sender_img !== "" ? (
-                      <Image
-                        style={styles.defaultImage}
-                        source={{ uri: notif.sender_img }}
-                        resizeMode="cover"
-                      />
-                    ) : (
-                      <FontAwesome5 name="user" size={40} color="black" />
-                    )}
-                  </View>
-                  <View style={styles.notifTextContainer}>
-                    <Text style={styles.notifTitle}>
-                      {notif.type === NotificationType.FRIEND_REQUEST
-                        ? "NEW FOLLOWER"
-                        : ""}
-                      {notif.type === NotificationType.COMMENT
-                        ? "NEW COMMENT"
-                        : ""}
-                      {notif.type === NotificationType.LIKE ? "NEW LIKE" : ""}
-                    </Text>
-                    <Text style={styles.notifMessage}>
-                      <Text style={{ fontWeight: "bold" }}>
-                        {notif.sender_name}
-                      </Text>
-                      <Text>
-                        {" "}
-                        {notif.message}
-                        {notif.type === NotificationType.COMMENT
-                          ? notif.comment
-                          : ""}{" "}
-                      </Text>
-                      <Text style={{ color: "grey" }}>{time}</Text>
-                    </Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
+              <NotificationItem key={notif.created.toString()} notif={notif} />
             );
           })}
         </ScrollView>
@@ -167,24 +106,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
     flexWrap: "wrap",
   },
-  defaultImageContainer: {
-    backgroundColor: "#d3d3d3",
-    height: 55,
-    width: 55,
-    borderColor: "black",
-    borderRadius: 30,
-    borderWidth: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 20,
-    alignSelf: "flex-start",
-    marginLeft: 5,
-  },
-  defaultImage: {
-    height: "100%", // Adjust the size of the image as needed
-    width: "100%", // Adjust the size of the image as needed
-    borderRadius: 30, // Make the image circular
-  },
   loading: {
     flex: 1,
     alignItems: "center",
@@ -194,22 +115,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-  },
-  notifTextContainer: {
-    flexDirection: "column",
-    flex: 1,
-  },
-  notifTitle: {
-    fontWeight: "bold",
-    fontSize: 18,
-    paddingLeft: 20,
-    paddingBottom: 5,
-  },
-  notifMessage: {
-    paddingLeft: 20,
-    flexWrap: "wrap",
-    flex: 1,
-    fontSize: 16,
   },
 });
 
