8c0accf6816b3c074b8fa7224ce3286c2debe727
Riley C <rileycrahen@gmail.com>
2024-09-09 10:37:03 -0600

    individual like disable, correct re-render

--- BookWorm/components/post/post.tsx
+++ BookWorm/components/post/post.tsx
@@ -1,18 +1,8 @@
-import { FontAwesome5 } from "@expo/vector-icons";
 import { type Timestamp } from "firebase/firestore";
-import React, { useState } from "react";
-import {
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { type PostModel } from "../../types";
-import { useAuth } from "../auth/context";
-import Comment from "../comment/comment";
+import LikeComment from "./LikeComment";
 import { usePostsContext } from "./PostsContext";
 import PagesProgressBar from "./ProgressBar/PagesProgressBar";
 import { areValidPageNumbers, formatDate } from "./util/postUtils";
@@ -25,7 +15,6 @@ interface PostProps {
   presentComments: (postID: string) => void;
 }
 
-// Using memo here makes it so it re-renders only when the props passed to it change
 const Post = ({
   post,
   created,
@@ -33,10 +22,7 @@ const Post = ({
   individualPage,
   presentComments,
 }: PostProps) => {
-  const { user } = useAuth();
-  const { posts, likePost, isLikePending, commentOnPost } = usePostsContext();
-  const [showCommentSection, setShowComments] = useState(individualPage);
-  const [newComment, setNewComment] = useState("");
+  const { posts } = usePostsContext();
   const formattedDate = formatDate(created, currentDate);
   const currentPost = posts.find((p) => p.id === post.id);
 
@@ -72,76 +58,12 @@ const Post = ({
           ))}
         </ScrollView>
       )}
-      <View style={styles.buttonrow}>
-        <TouchableOpacity
-          disabled={isLikePending}
-          style={[styles.likebutton, isLikePending && styles.pendingOpacity]}
-          onPress={() => {
-            likePost(post.id);
-          }}
-        >
-          {post.likes.includes(user?.uid ?? "") ? (
-            <FontAwesome5 name="heart" solid size={15} color="red" />
-          ) : (
-            <FontAwesome5 name="heart" size={15} />
-          )}
-        </TouchableOpacity>
-        <Text
-          style={[styles.textPadding, isLikePending && styles.pendingOpacity]}
-        >
-          {post.likes.length}
-          {post.likes.length === 1 ? " Like" : " Likes"}
-        </Text>
-        <TouchableOpacity
-          style={styles.likebutton}
-          onPress={() => {
-            if (individualPage) {
-              setShowComments(!showCommentSection);
-            } else {
-              presentComments(post.id);
-            }
-          }}
-        >
-          <FontAwesome5 name="comment" size={15} />
-          <Text style={{ paddingLeft: 5 }}>
-            {post.comments.length}
-            {post.comments.length === 1 ? " Comment" : " Comments"}
-          </Text>
-        </TouchableOpacity>
-      </View>
-      {showCommentSection && (
-        <>
-          <FlatList
-            data={post.comments}
-            renderItem={({ item: comment }) => <Comment comment={comment} />}
-          />
-          <View style={styles.commentInputContainer}>
-            <TextInput
-              style={styles.commentInput}
-              value={newComment}
-              placeholder={`Add a comment to ${post.user.first}'s post`}
-              autoCapitalize="none"
-              multiline
-              onChangeText={(text) => {
-                setNewComment(text);
-              }}
-            />
-            <TouchableOpacity
-              style={[
-                styles.button,
-                newComment.trim() === "" && styles.buttonDisabled,
-              ]}
-              onPress={() => {
-                commentOnPost(post.id, newComment);
-                setNewComment("");
-              }}
-              disabled={newComment.trim() === ""}
-            >
-              <Text style={styles.buttonText}>Comment</Text>
-            </TouchableOpacity>
-          </View>
-        </>
-      )}
+      <LikeComment
+        post={post}
+        key={`${post.id}-${post.comments.length}-${post.likes.length}`}
+        individualPage={individualPage}
+        presentComments={presentComments}
+      />
     </View>
   );
 };
