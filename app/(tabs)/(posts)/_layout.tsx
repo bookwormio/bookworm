20b43adc2005fa3b4f79cca2e88cf2327fc35b53
nforr <u1310549@utah.edu>
2024-09-26 11:10:42 -0600

    fixed for the comments

--- app/(tabs)/(posts)/_layout.tsx
+++ app/(tabs)/(posts)/_layout.tsx
@@ -1,79 +1,89 @@
 import { FontAwesome5 } from "@expo/vector-icons";
 import { router, Stack } from "expo-router";
 import React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import BackButton from "../../../components/backbutton/BackButton";
 
 const PostsLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name="posts"
-        options={{
-          headerTitle: "Posts",
-          headerShown: true,
-          headerRight: () => (
-            <TouchableOpacity
-              style={styles.newPost}
-              onPress={() => {
-                router.push({ pathname: "notifications" });
-              }}
-              disabled={false}
-            >
-              <FontAwesome5 name="bell" size={20} color="#FB6D0B" />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="[postID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Post",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="user/[friendUserID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Friend Profile",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="notifications"
-        options={{
-          headerShown: true,
-          headerTitle: "Notifications",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="postsbook/[bookID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Book",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="postsfollow/[friendUserID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Follow Details",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="viewposts/[postID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Post",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-    </Stack>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Stack>
+        <Stack.Screen
+          name="posts"
+          options={{
+            headerTitle: "Posts",
+            headerShown: true,
+            headerRight: () => (
+              <TouchableOpacity
+                style={styles.newPost}
+                onPress={() => {
+                  router.push({ pathname: "notifications" });
+                }}
+                disabled={false}
+              >
+                <FontAwesome5 name="bell" size={20} color="#FB6D0B" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="[postID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Post",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="user/[friendUserID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Friend Profile",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="notifications"
+          options={{
+            headerShown: true,
+            headerTitle: "Notifications",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="postsbook/[bookID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Book",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="postsfollow/[friendUserID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Follow Details",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="viewposts/[postID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Post",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+      </Stack>
+    </KeyboardAvoidingView>
   );
 };
 
