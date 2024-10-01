686b68dbfaab3db7f66e8bb59ed57ebac927527a
nforr <u1310549@utah.edu>
2024-09-30 20:25:01 -0600

    added keyboard avoiding to search

--- app/(tabs)/(search)/_layout.tsx
+++ app/(tabs)/(search)/_layout.tsx
@@ -1,46 +1,52 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import BackButton from "../../../components/backbutton/BackButton";
 
 const SearchLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name="search"
-        options={{ headerShown: true, headerTitle: "Search" }}
-      />
-      <Stack.Screen
-        name="user/[friendUserID]"
-        options={{
-          headerShown: true,
-          headerTitle: "User",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="recommendation/[friendUserID]"
-        options={{
-          presentation: "modal",
-          headerTitle: "Leave a Recommendation",
-        }}
-      />
-      <Stack.Screen
-        name="searchbook/[bookID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Book",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-      <Stack.Screen
-        name="posts/[postID]"
-        options={{
-          headerShown: true,
-          headerTitle: "Post",
-          headerLeft: () => <BackButton />,
-        }}
-      />
-    </Stack>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Stack>
+        <Stack.Screen
+          name="search"
+          options={{ headerShown: true, headerTitle: "Search" }}
+        />
+        <Stack.Screen
+          name="user/[friendUserID]"
+          options={{
+            headerShown: true,
+            headerTitle: "User",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="recommendation/[friendUserID]"
+          options={{
+            presentation: "modal",
+            headerTitle: "Leave a Recommendation",
+          }}
+        />
+        <Stack.Screen
+          name="searchbook/[bookID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Book",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+        <Stack.Screen
+          name="posts/[postID]"
+          options={{
+            headerShown: true,
+            headerTitle: "Post",
+            headerLeft: () => <BackButton />,
+          }}
+        />
+      </Stack>
+    </KeyboardAvoidingView>
   );
 };
 
