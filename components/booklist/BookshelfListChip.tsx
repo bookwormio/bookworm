fbe5821129d2c5e467f4214192295cf86e0f97ba
Riley C <rileycrahen@gmail.com>
2024-10-25 10:10:24 -0600

    cleanup

--- components/booklist/BookshelfListChip.tsx
+++ components/booklist/BookshelfListChip.tsx
@@ -1,9 +1,10 @@
 import { Chip } from "@rneui/themed";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
+import { StyleSheet } from "react-native";
 import {
-  REMAINING_PROGRESS_COLOR,
-  SECOND_PROGRESS_COLOR,
+  BORROWING_CHIP_COLORS,
+  CHIP_GRADIENT_COLORS,
 } from "../../constants/constants";
 import { BOOKSHELF_CHIP_DISPLAY, ServerBookShelfName } from "../../enums/Enums";
 
@@ -11,38 +12,24 @@ interface BookshelfListChipProps {
   bookShelf: ServerBookShelfName;
 }
 
-interface chipStyle {
-  textColor: string;
+interface ChipStyle {
   chipColor: string[];
 }
 
 const BookshelfListChip = ({ bookShelf }: BookshelfListChipProps) => {
-  // TODO finalize these colors
-  // TODO move these colors to constants
-  const lightcolor = "#FFA669";
-  const darkercolor = "#FF8D40";
-
-  const READING_CHIP_COLORS = [SECOND_PROGRESS_COLOR, SECOND_PROGRESS_COLOR];
-  const WANT_TO_READ_CHIP_COLORS = [
-    REMAINING_PROGRESS_COLOR,
-    REMAINING_PROGRESS_COLOR,
-  ];
-  const FINISHED_CHIP_COLORS = [lightcolor, darkercolor];
-  const LENDING_CHIP_COLORS = ["#EE876B", "#EE876B"];
-  const BORROWING_CHIP_COLORS = ["#455194", "#455194"];
-
-  const getChipStyles = (bookShelf: ServerBookShelfName): chipStyle => {
+  const getChipStyles = (bookShelf: ServerBookShelfName): ChipStyle => {
+    // TODO: Remove switch once we have borrowing library
     switch (bookShelf) {
       case ServerBookShelfName.CURRENTLY_READING:
-        return { textColor: "white", chipColor: READING_CHIP_COLORS };
+        return { chipColor: CHIP_GRADIENT_COLORS[bookShelf] };
       case ServerBookShelfName.WANT_TO_READ:
-        return { textColor: "black", chipColor: WANT_TO_READ_CHIP_COLORS };
+        return { chipColor: CHIP_GRADIENT_COLORS[bookShelf] };
       case ServerBookShelfName.FINISHED:
-        return { textColor: "white", chipColor: FINISHED_CHIP_COLORS };
+        return { chipColor: CHIP_GRADIENT_COLORS[bookShelf] };
       case ServerBookShelfName.LENDING_LIBRARY:
-        return { textColor: "white", chipColor: LENDING_CHIP_COLORS };
+        return { chipColor: CHIP_GRADIENT_COLORS[bookShelf] };
       default:
-        return { textColor: "white", chipColor: BORROWING_CHIP_COLORS };
+        return { chipColor: BORROWING_CHIP_COLORS };
     }
   };
 
@@ -52,32 +39,40 @@ const BookshelfListChip = ({ bookShelf }: BookshelfListChipProps) => {
     <Chip
       title={BOOKSHELF_CHIP_DISPLAY[bookShelf]}
       type="outline"
-      // TODO: move to stylesheet
-      containerStyle={{ marginVertical: 15 }}
+      containerStyle={styles.containerStyle}
       ViewComponent={LinearGradient} // allows for gradient background
       linearGradientProps={{
         colors: chipStyles.chipColor,
         start: { x: 0, y: 0.5 },
         end: { x: 1, y: 0.5 },
       }}
-      // TODO: move to stylesheet
-      titleStyle={{
-        paddingTop: 0,
-        paddingBottom: 0,
-        color: chipStyles.textColor,
-        opacity: chipStyles.textColor === "white" ? 1 : 0.6,
-        fontWeight: "bold",
-        textAlign: "center",
-        textAlignVertical: "top",
-        fontSize: 12,
-        shadowColor: "#000",
-        shadowOffset: { width: 1, height: 1 },
-        shadowOpacity: 0.3,
-      }}
-      // TODO: move to stylesheet
-      buttonStyle={{ borderWidth: 0, height: 25, padding: 0 }}
+      titleStyle={[styles.titleStyle]}
+      buttonStyle={styles.buttonStyle}
     />
   );
 };
 
 export default BookshelfListChip;
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    marginVertical: 15,
+  },
+  titleStyle: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    fontWeight: "bold",
+    textAlign: "center",
+    textAlignVertical: "top",
+    fontSize: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.3,
+    color: "white",
+  },
+  buttonStyle: {
+    borderWidth: 0,
+    height: 25,
+    padding: 0,
+  },
+});
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,5 @@
 import { type ImageStyle, type StyleProp } from "react-native";
+import { ServerBookShelfName } from "../enums/Enums";
 
 export const DAYS_OF_WEEK = [
   "Sunday",
@@ -48,11 +49,30 @@ export const NEW_POST_BOOK_SEARCH = "createbooksearch";
 
 export const BLURHASH = "LBN-4dMwIUWC~WRj%M^+-;4n4nWC";
 
-export const FIRST_PROGRESS_COLOR = "rgb(88, 166, 92)";
-export const SECOND_PROGRESS_COLOR = "rgb(169, 207, 96)";
+export const BOOKWORM_DARK_GREEN = "rgb(88, 166, 92)";
+export const BOOKWORM_LIGHT_GREEN = "rgb(169, 207, 96)";
 export const BACKWARDS_PROGRESS_COLOR = "rgb(255, 99, 71)";
 export const REMAINING_PROGRESS_COLOR = "rgb(229, 232, 249)";
 export const BOOK_WORM_ORANGE = "#FB6D0B";
+export const LIGHT_ORANGE = "#FFA669";
+export const MEDIUM_ORANGE = "#FF8D40";
+export const LIGHT_SALMON = "#FFAA93";
+export const DARK_SALMON = "#EE876B";
+export const LIGHT_LAVENDER = "#C0C7EA";
+export const DARK_LAVENDER = "#939CCF";
+export const MEDIUM_GREEN = "#A9CF60";
+export const LIGHT_GOLD = "#FFCC34";
+export const DARK_GOLD = "#F6B902";
+
+export const BORROWING_CHIP_COLORS = [LIGHT_GOLD, DARK_GOLD];
+
+export const CHIP_GRADIENT_COLORS = {
+  [ServerBookShelfName.CURRENTLY_READING]: [BOOKWORM_LIGHT_GREEN, MEDIUM_GREEN],
+  [ServerBookShelfName.WANT_TO_READ]: [LIGHT_LAVENDER, DARK_LAVENDER],
+  [ServerBookShelfName.FINISHED]: [LIGHT_ORANGE, MEDIUM_ORANGE],
+  [ServerBookShelfName.LENDING_LIBRARY]: [LIGHT_SALMON, DARK_SALMON],
+  // [ServerBookShelfName.BORROWING]: [LIGHT_GOLD, DARK_GOLD],
+};
 
 export const POST_IMAGE_HEIGHT = 250;
 export const POST_IMAGE_WIDTH = 250;
