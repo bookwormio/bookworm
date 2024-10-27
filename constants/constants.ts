2239809abb0e0b66c5431dd1a057c1541dfd1bd2
Riley C <rileycrahen@gmail.com>
2024-10-27 12:32:41 -0600

    update colors

--- constants/constants.ts
+++ constants/constants.ts
@@ -50,7 +50,7 @@ export const NEW_POST_BOOK_SEARCH = "createbooksearch";
 export const BLURHASH = "LBN-4dMwIUWC~WRj%M^+-;4n4nWC";
 
 export const BOOKWORM_DARK_GREEN = "rgb(88, 166, 92)";
-export const BOOKWORM_LIGHT_GREEN = "rgb(169, 207, 96)";
+export const BOOKWORM_LIGHT_GREEN = "#A9CF60";
 export const BACKWARDS_PROGRESS_COLOR = "rgb(255, 99, 71)";
 export const REMAINING_PROGRESS_COLOR = "rgb(229, 232, 249)";
 export const BOOK_WORM_ORANGE = "#FB6D0B";
@@ -58,19 +58,19 @@ export const LIGHT_ORANGE = "#FFA669";
 export const MEDIUM_ORANGE = "#FF8D40";
 export const LIGHT_SALMON = "#FFAA93";
 export const DARK_SALMON = "#EE876B";
-export const LIGHT_LAVENDER = "#C0C7EA";
-export const DARK_LAVENDER = "#939CCF";
-export const MEDIUM_GREEN = "#A9CF60";
+export const MEDIUM_GREEN = "#84AC38";
 export const LIGHT_GOLD = "#FFCC34";
 export const DARK_GOLD = "#F6B902";
+export const DEEP_BLUE = "#417CE0";
+export const LIGHT_BLUE = "#7CA4E9";
 
 export const BORROWING_CHIP_COLORS = [LIGHT_GOLD, DARK_GOLD];
 
 export const CHIP_GRADIENT_COLORS = {
-  [ServerBookShelfName.CURRENTLY_READING]: [BOOKWORM_LIGHT_GREEN, MEDIUM_GREEN],
-  [ServerBookShelfName.WANT_TO_READ]: [LIGHT_LAVENDER, DARK_LAVENDER],
-  [ServerBookShelfName.FINISHED]: [LIGHT_ORANGE, MEDIUM_ORANGE],
-  [ServerBookShelfName.LENDING_LIBRARY]: [LIGHT_SALMON, DARK_SALMON],
+  [ServerBookShelfName.CURRENTLY_READING]: [LIGHT_BLUE, DEEP_BLUE], // #7CA4E9, #417CE0
+  [ServerBookShelfName.WANT_TO_READ]: [LIGHT_ORANGE, MEDIUM_ORANGE], // #FFA669, #FF8D40
+  [ServerBookShelfName.FINISHED]: [BOOKWORM_LIGHT_GREEN, MEDIUM_GREEN], // #A9CF60, #84AC38
+  [ServerBookShelfName.LENDING_LIBRARY]: [LIGHT_SALMON, DARK_SALMON], // #FFAA93, #EE876B
   // TODO: update once we have borrowing library
   // [ServerBookShelfName.BORROWING]: [LIGHT_GOLD, DARK_GOLD],
 };
