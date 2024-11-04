70bbc837514b7915b50344679c2e12bf384f7184
Sam Christensen <samctensen@gmail.com>
2024-11-03 19:45:01 -0700

    frankenstein

--- app/(tabs)/(create)/NewPost.tsx
+++ app/(tabs)/(create)/NewPost.tsx
@@ -195,7 +195,6 @@ const NewPost = () => {
   const pickImageAsync = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      aspect: [1, 1],
       quality: 1,
     });
     if (!result.canceled) {
