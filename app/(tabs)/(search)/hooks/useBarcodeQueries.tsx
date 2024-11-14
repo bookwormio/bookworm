cb74963f5d5759319b28a6a60d1104cb8158baa4
Sam Christensen <samctensen@gmail.com>
2024-11-13 19:08:48 -0700

    better mutation comment

--- app/(tabs)/(search)/hooks/useBarcodeQueries.tsx
+++ app/(tabs)/(search)/hooks/useBarcodeQueries.tsx
@@ -5,11 +5,8 @@ import { fetchBookVolumeIDByISBN } from "../../../../services/books-services/Boo
  * Custom hook to fetch a books volumeID from a provided ISBN string.
  *
  * @param {string} isbn - The isbn of the book whose volumeID is being fetched.
- * @returns {UseMutationResult<string | null>} The result of the query, containing the profile picture URL or null.
+ * @returns {UseMutationResult<string | null>} The result of the query, containing the volumeID or null.
  *
- * @example
- * const { data: profilePicUrl, isLoading, isError, error } = useProfilePicQuery(userId);
- * // profilePicUrl is of type string | null
  */
 export const useVolumeIDMutation = () => {
   return useMutation({
